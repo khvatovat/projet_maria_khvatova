4c32f8800eb9759582176490d5c484d503cc0cc2
maria.khvatova@epita.fr <maria.khvatova@epita.fr>
2025-11-20 14:44:43 +0100

    feat : basic skeleton

---
+++ projet_maria_khvatova/src/main/java/org/formation/projet_maria_khvatova/ProjetMariaKhvatovaApplication.java
@@ -0,0 +1,13 @@
+package org.formation.projet_maria_khvatova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMariaKhvatovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetMariaKhvatovaApplication.class, args);
+    }
+
+}
---
+++ projet_maria_khvatova/src/test/java/org/formation/projet_maria_khvatova/ProjetMariaKhvatovaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.projet_maria_khvatova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMariaKhvatovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
